3439092802a560c8ebbe4a9dfd189e098e5334e1
Ezekiel Nwuguru <eazy4life2017@gmail.com>
2023-09-10 23:59:15 +0100

    initial commit

---
+++ task1/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/api', (req, res)=> {
+    const {slack_name, track} = req.query
+    const date = new Date()
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    const utcDate = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDay()))
+    res.json({ 
+    slack_name,
+    current_day: weekday[date.getDay()],
+    utc_time: utcDate,
+    track,
+    github_file_url: "https://github.com/username/repo/blob/main/file_name.ext",
+    github_repo_url: "https://github.com/username/repo",
+    status_code: 200
+    })
+})
+
+app.listen(8000, ()=>{
+    console.log("Server running....")
+})
